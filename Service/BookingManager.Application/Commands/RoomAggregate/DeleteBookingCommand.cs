9c4b6bf049b867b36924a87e54d635ff506a6353
Trishna Pattanaik <trishna.PATTANAIK@arcstone.co>
2025-04-27 13:25:11 +0530

    update status on delete and update command

--- Service/BookingManager.Application/Commands/RoomAggregate/DeleteBookingCommand.cs
+++ Service/BookingManager.Application/Commands/RoomAggregate/DeleteBookingCommand.cs
@@ -4,6 +4,8 @@ using MediatR;
 using BookingManager.Domain.Aggregates.BookingAggregate;
 using BookingManager.Domain.Exceptions;
 using BookingManager.Domain.Repositories;
+using BookingManager.Application.Commands.DataTransferObjects;
+using BookingManager.Application.Helpers;
 
 namespace BookingManager.Application.Commands.BookingAggregate
 {
@@ -19,16 +21,24 @@ namespace BookingManager.Application.Commands.BookingAggregate
     public class DeleteBookingCommandCommandHandler : IRequestHandler<DeleteBookingCommand>
     {
         private readonly IBookingRepository _repository;
+        private readonly KafkaProducerHelper _kafkaProducer;
 
-        public DeleteBookingCommandCommandHandler(IBookingRepository repository)
+        public DeleteBookingCommandCommandHandler(IBookingRepository repository, KafkaProducerHelper kafkaProducer)
         {
             _repository = repository;
+            _kafkaProducer = kafkaProducer;
         }
         public async Task Handle(DeleteBookingCommand command, CancellationToken cancellationToken)
         {
-            Booking Booking = await _repository.GetAsync(command.Id) ?? throw new BookingNotFoundException(command.Id);
-            Booking.Delete();
+            Booking booking = await _repository.GetAsync(command.Id) ?? throw new BookingNotFoundException(command.Id);
+            booking.Delete();
             await _repository.UnitOfWork.SaveEntitiesAsync();
+            await _kafkaProducer.ProduceAsync(new RoomAvailabilityDto()
+            {
+                Status = "Available",
+                RoomId = booking.RoomId,
+                Type = "Guest"
+            });
         }
     }
 }
--- Service/BookingManager.Application/Commands/RoomAggregate/UpdateBookingCommand.cs
+++ Service/BookingManager.Application/Commands/RoomAggregate/UpdateBookingCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using BookingManager.Application.Commands.DataTransferObjects;
+using BookingManager.Application.Helpers;
 using BookingManager.Domain.Aggregates.BookingAggregate;
 using BookingManager.Domain.Exceptions;
 using BookingManager.Domain.Repositories;
@@ -15,16 +17,27 @@ namespace BookingManager.Application.Commands.BookingAggregate
     public class UpdateRoomCommandCommandHandler : IRequestHandler<UpdateBookingCommand>
     {
         private readonly IBookingRepository _repository;
+        private readonly KafkaProducerHelper _kafkaProducer;
 
-        public UpdateRoomCommandCommandHandler(IBookingRepository repository)
+        public UpdateRoomCommandCommandHandler(IBookingRepository repository, KafkaProducerHelper kafkaProducer)
         {
             _repository = repository;
+            _kafkaProducer = kafkaProducer;
         }
         public async Task Handle(UpdateBookingCommand command, CancellationToken cancellationToken)
         {
             Booking booking = await _repository.GetAsync(command.Id) ?? throw new BookingNotFoundException(command.Id);
             booking.UpdateStatus(command.Status);
             await _repository.UnitOfWork.SaveEntitiesAsync();
+            if (command.Status.ToLower() == "checkout" || command.Status.ToLower() == "cancel")
+            {
+                await _kafkaProducer.ProduceAsync(new RoomAvailabilityDto()
+                {
+                    Status = "Available",
+                    RoomId = booking.RoomId,
+                    Type = "Guest"
+                });
+            }
         }
     }
 }
